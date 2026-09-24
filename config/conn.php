035d71a55fcde9b42cbfd116e1995a5151752144
ebinsharuddin-svg <ebinsharuddin@gmail.com>
2026-09-24 11:42:32 +0800

    Fix connection string in conn.php
    Removed extraneous text from connection string initialization.

--- config/conn.php
+++ config/conn.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "foodorder_system";
-$conn = new mysqli("localhost", "root", "", "foodorder_system");
+$conn = new mysqli("localhost", "root", "", "foodorder_system");dsdsdsds
 
 if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
