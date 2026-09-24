f794bcaaea4142d6e2712792c53544110e34dbe0
ebinsharuddin-svg <ebinsharuddin@gmail.com>
2026-09-24 11:43:33 +0800

    Clean up connection code in conn.php
    Removed extraneous text and added a comment.

--- config/conn.php
+++ config/conn.php
@@ -3,7 +3,8 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "foodorder_system";
-$conn = new mysqli("localhost", "root", "", "foodorder_system");dsdsdsds
+$conn = new mysqli("localhost", "root", "", "foodorder_system");
+//THIS CODE IS ASSS
 
 if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
